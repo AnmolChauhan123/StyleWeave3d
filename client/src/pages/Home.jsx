964e1150693e094e160ac385283aea6a54d6e921
Anmol Chauhan <77499508+AnmolChauhan123@users.noreply.github.com>
2023-08-25 22:33:08 +0530

    First Commit

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,60 @@
+import { motion, AnimatePresence } from 'framer-motion';
+import { useSnapshot } from 'valtio';
+
+import state from '../store';
+import { CustomButton } from '../components';
+import {
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation
+} from '../config/motion';
+
+const Home = () => {
+  const snap = useSnapshot(state);
+
+  return (
+    <AnimatePresence>
+      {snap.intro && (
+        <motion.section className="home" {...slideAnimation('left')}>
+          <motion.header {...slideAnimation("down")}>
+            <img 
+              src='./threejs.png'
+              alt="logo"
+              className="w-8 h-8 object-contain"
+            />
+          </motion.header>
+
+          <motion.div className="home-content" {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>
+              <h1 className="head-text">
+                LET'S <br className="xl:block hidden" />
+                
+                <div class="bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% text-[#efd048]">DO IT.</div>
+                
+                 
+              </h1>
+            </motion.div>
+            <motion.div
+              {...headContentAnimation}
+              className="flex flex-col gap-5"
+            >
+              <p className="max-w-md font-normal text-gray-600 text-base">
+              <strong>Experience</strong> the <strong>unparalleled </strong>opportunity to design your very own one-of-a-kind shirt using our revolutionary 3D customization tool. Set <strong>your creativity free</strong> and establish a distinctive fashion statement that is entirely unique to you.
+              </p>
+
+              <CustomButton 
+                type="filled"
+                title="Customize It"
+                handleClick={() => state.intro = false}
+                customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+              />
+            </motion.div>
+          </motion.div>
+        </motion.section>
+      )}
+    </AnimatePresence>
+  )
+}
+
+export default Home;
